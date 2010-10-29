2d77a022e4d739d308a4f7178c7c57e9b727299f
phoumphan <phil.houmphan@gmail.com>
2010-10-29 15:11:17 -0700

    another commit to the baseline

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.resources :users
-
+  map.resources :events
   map.resource :session
 
   # The priority is based upon order of creation: first created -> highest priority.
