38c59a275f5dfa954a34ea82e6fd62ccd676f689
phoumphan <phil.houmphan@gmail.com>
2010-11-08 13:32:22 -0800

    forgot to add some files to the last event creation commit

---
+++ app/controllers/eventtypes_controller.rb
@@ -0,0 +1,20 @@
+class EventtypesController < ApplicationController
+
+  def new
+    @eventtype = Eventtype.new
+    @user_eventtypes = eventtype.user_eventtypes.new
+    @event_eventtypes = eventtype.event_eventtypes.new    
+  end
+
+  
+  def select_for_event
+    	@eventtypes = Eventtype.find(:all, :conditions => [ 'LOWER(description) LIKE ?', '%' + params[:q].downcase + '%' ],  :order => 'description ASC', :limit => 8)
+      @eventtypes.each {|e| puts e.description}
+    	@eventtypes_hash = @eventtypes.collect! { |x| {"name" => x.description, "id" => x.id } }
+      
+      render :partial => "eventtypes/list_for_select"
+      
+      
+  end
+
+end
