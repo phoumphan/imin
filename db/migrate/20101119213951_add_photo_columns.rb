54a56a5b48db6cd13d83cf175883f001d3560a02
James <jacinabox@hotmail.com>
2010-11-19 15:22:51 -0800

    Untested photo code

---
+++ db/migrate/20101119213951_add_photo_columns.rb
@@ -0,0 +1,23 @@
+# Based on https://github.com/thoughtbot/paperclip
+
+class AddPhotoColumns < ActiveRecord::Migration
+
+
+  def self.up
+    remove_column :photos, :photo_url
+    add_column :photos, :photo_file_name,    :string
+    add_column :photos, :photo_content_type, :string
+    add_column :photos, :photo_file_size,    :integer
+    add_column :photos, :photo_updated_at,   :datetime
+  end
+
+  def self.down
+    add_column :photos, :photo_url,       :string
+    remove_column :photos, :photo_file_name
+    remove_column :photos, :photo_content_type
+    remove_column :photos, :photo_file_size
+    remove_column :photos, :photo_updated_at
+  end
+en
+
+d
